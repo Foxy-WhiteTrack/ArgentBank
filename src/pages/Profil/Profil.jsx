384f19ffab3f686b1b7a93e1ea0c8297f0630f9b
Foxy WhiteTrack | One Trick <48949675+Foxy-WhiteTrack@users.noreply.github.com>
2023-11-30 17:59:59 +0100

    Mise à jour du nom et prénom OP

--- src/pages/Profil/Profil.jsx
+++ src/pages/Profil/Profil.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import './Profil.css';
 
 import { Account } from '../../components/Account/Account';
@@ -6,21 +6,49 @@ import accountData from '../../mock/accountData';
 
 import { useNavigate } from 'react-router-dom';
 import stockData from '../../services/stockData';
+import callsApi from '../../services/callsApi';
 
 import { useDispatch, useSelector } from 'react-redux';
+import { setFirstName, setLastName } from '../../redux/slice';
 
 export default function Profil() {
 
-    const dispatch = useDispatch();
-    const navigate = useNavigate();
     const { firstName, lastName, isAuthenticated } = useSelector((state) => state.user);
 
-    // useEffect(() => {
-    //     if (!isAuthenticated) {
-    //         navigate('/login');
-    //     }
-    // }, [isAuthenticated, navigate]);
+    const [editionFirstName, setEditFirstName] = useState(firstName || "");
+    const [editionLastName, setEditLastName] = useState(lastName || "");
+
+    const [isFormDisplayed, setIsFormDisplayed] = useState(false);
+
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        if (!isAuthenticated) {
+            navigate('/login');
+        }
+    }, [isAuthenticated, navigate]);
+
     const getAuth = stockData.getAuthentication();
+
+    const handleChange = async (e) => {
+        e.preventDefault();
+        const result = await callsApi.updateUser(getAuth.token, editionFirstName, editionLastName);
+        if (result) {
+            console.log('success');
+            setIsFormDisplayed(false);
+            dispatch(setFirstName(editionFirstName));
+            dispatch(setLastName(editionLastName));
+        } else {
+            console.error(result.message);
+        }
+
+    };
+
+    const resetEdition = () => {
+        setEditFirstName(firstName || "");
+        setEditLastName(lastName || "");
+    };
 
     return (
         <>
@@ -28,7 +56,26 @@ export default function Profil() {
                 <div className="header2">
                     {/* Utiliser les données du profil */}
                     <h1>Welcome back<br />{firstName} {lastName} !</h1>
-                    <button className="edit-button">Edit Name</button>
+                    {isFormDisplayed ? (
+                        <>
+                            <form className="edit" onSubmit={handleChange}>
+                                <input
+                                    value={editionFirstName}
+                                    onChange={(e) => setEditFirstName(e.target.value)}
+                                    placeholder={firstName}
+                                />
+                                <input
+                                    value={editionLastName}
+                                    onChange={(e) => setEditLastName(e.target.value)}
+                                    placeholder={lastName}
+                                />
+                                <button className="edit-button" type='submit'>Save</button>
+                                <button className="edit-button cancel" onClick={() => { setIsFormDisplayed(false); resetEdition(); }}>Cancel</button>
+                            </form>
+                        </>
+                    ) : (
+                        <button className="edit-button" onClick={() => setIsFormDisplayed(true)}>Edit Name</button>
+                    )}
                 </div>
                 <h2 className="sr-only">Accounts</h2>
 
--- src/services/callsApi.js
+++ src/services/callsApi.js
@@ -55,6 +55,31 @@ const fetchApi = {
             return null;
         }
     },
+
+    // Mettre à jour le profil de l'user
+    async updateUser(token, firstName, lastName) {
+        try {
+            const response = await fetch(`${BASE_URL}/profile`, {
+                method: 'PUT',
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({ firstName, lastName }),
+            });
+
+            const data = await response.json();
+
+            if (response.status === 200) {
+                return data.body;
+            } else {
+                throw new Error(data.message);
+            }
+        } catch (error) {
+            console.error('Erreur lors de la mise à jour du profil:', error);
+            return null;
+        }
+    },
 };
 
 export default fetchApi;
